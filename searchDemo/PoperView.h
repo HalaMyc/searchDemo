879483599885dad3660cb012b45cf2d52fe8e4c2
牟亚诚 <halamyc@qq.com>
2015-09-21 19:57:13 +0800

    初步完成，接口还有问题

---
+++ searchDemo/PoperView.h
@@ -0,0 +1,13 @@
+//
+//  PoperView.h
+//  searchDemo
+//
+//  Created by 牟亚诚 on 15/9/21.
+//  Copyright © 2015年 牟亚诚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PoperView : UIView
+
+@end
---
+++ searchDemo/PoperView.m
@@ -0,0 +1,21 @@
+//
+//  PoperView.m
+//  searchDemo
+//
+//  Created by 牟亚诚 on 15/9/21.
+//  Copyright © 2015年 牟亚诚. All rights reserved.
+//
+
+#import "PoperView.h"
+
+@implementation PoperView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
